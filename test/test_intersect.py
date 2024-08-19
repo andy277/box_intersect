24856674499131c746cd232cac6761900880c3fa
Andy Nguyen <andy@Andys-MacBook-Pro.local>
2024-08-19 08:03:36 -0700

    finish code in test

---
+++ test/test_intersect.py
@@ -0,0 +1,51 @@
+import unittest
+from typing import Tuple
+
+class TestIntersect(unittest.TestCase):
+    # def convert_box(self,  rect: Tuple[float, ...]) -> Tuple[float, ...]:
+    #     x_max = rect[0] + rect[2] / 2
+    #     y_max = rect[1] + rect[3] / 2
+    #     x_min = rect[0] - rect[2] / 2
+    #     y_min = rect[1] - rect[3] / 2
+    #     return x_min, y_min, x_max, y_max
+
+    def intersection(self, rect1: Tuple[float, ...], rect2: Tuple[float, ...]) -> float:
+        self.assertIsNotNone(rect1)
+        self.assertIsNotNone(rect2)
+
+        rect1_xmax = rect1[0] + rect1[2]/2
+        rect2_xmax = rect2[0] + rect2[2]/2
+        rect1_xmin = rect1[0] - rect1[2]/2
+        rect2_xmin = rect2[0] - rect2[2]/2
+
+        rect1_ymax = rect1[1] + rect1[2]/2
+        rect2_ymax = rect2[1] + rect2[2]/2
+        rect1_ymin = rect1[1] - rect1[2]/2
+        rect2_ymin = rect2[1] - rect2[2]/2
+
+        dx = min(rect1_xmax, rect2_xmax) - max(rect1_xmin, rect2_xmin)
+        dy = min(rect1_ymax, rect2_ymax) - max(rect1_ymin, rect2_ymin)
+
+        if dx >= 0 and dy >= 0:
+            return dx * dy
+
+    def total_area(self, rect1: Tuple[float, ...], rect2: Tuple[float, ...]) -> float:
+        rect1_area = rect1[2] * rect1[3]
+        rect2_area = rect2[2] * rect2[3]
+
+        return rect1_area + rect2_area
+
+    def test_main(self):
+        rect1 = (0.48, 0.63, 0.69, 0.71)
+        rect2 = (0.74, 0.52, 0.31, 0.93)
+        print(self.intersection(rect1, rect2))
+        print(self.total_area(rect1, rect2))
+
+
+t = TestIntersect()
+
+# t.test_main()
+
+# print(t.intersection((0.48, 0.63, 0.69, 0.71), (0.74, 0.52, 0.31, 0.93)))
+
+# (0.74, 0.52, 0.31, 0.93))
